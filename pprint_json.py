0a875117e7fcd78ff4bf587399f2a2a727b5af0a
Wasja Ostrowsky <ostrowskyi@gmail.com>
2016-12-20 13:39:35 +0300

    Задание выполнено

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,16 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='UTF-8') as f:
+        lines = f.read()
+        return json.loads(lines)
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, ensure_ascii = False, sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input("Введите путь к файлу\n")
+    data = load_data(filepath)
+    print(pretty_print_json(data))
